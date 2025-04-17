943aa548de99a4523c951ddf07e62d83d119b5bf
NITHINSAI <149461948+NITHIN-SAI-SRM@users.noreply.github.com>
2025-04-17 18:56:55 +0530

    Update config.php

--- ISM/ism/config/config.php
+++ ISM/ism/config/config.php
@@ -1,6 +1,6 @@
     <?php
-	    define("DB_HOST", "localhost");
-	    define("DB_USER", "root");
-	    define("DB_PASS", "");
-	    define("DB_NAME", "db_sams");
+	    define("DB_HOST", "attendance.mysql.database.azure.com");
+	    define("DB_USER", "abc123@attendance");
+	    define("DB_PASS", "123456@aB");
+	    define("DB_NAME", "attendance");
     ?>
