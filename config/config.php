3259baaf17e6e3811f6427ffc188e5bac3a6e732
NITHINSAI <149461948+NITHIN-SAI-SRM@users.noreply.github.com>
2025-04-17 19:40:50 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
     <?php
 	    define("DB_HOST", "attendance.mysql.database.azure.com");
-	    define("DB_USER", "abc123@attendance");
+	    define("DB_USER", "abc123");
 	    define("DB_PASS", "123456@aB");
 	    define("DB_NAME", "attendance");
     ?>
